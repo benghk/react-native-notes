07227b5e520aa376284f84233969b1b34a30b854
bbk1985 <ben-@live.com>
2016-02-03 15:22:54 +0800

    Created simple button component

---
+++ App/Components/SimpleButton.js
@@ -0,0 +1,10 @@
+import React, {
+  Text,
+  View
+} from 'react-native';
+
+export default class SimpleButton extends React.Component {
+  render() {
+    return (<View><Text>Simple button</Text></View>);
+  }
+}
--- index.android.js
+++ index.android.js
@@ -12,20 +12,13 @@ var {
   View,
 } = React;
 
+import SimpleButton from './App/Components/SimpleButton';
+
 var notes = React.createClass({
   render: function() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <SimpleButton/>
       </View>
     );
   }
@@ -37,16 +30,6 @@ var styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
 
