9e234344ea492bcb560ada9769e55314f0cd9c92
bbk1985 <ben-@live.com>
2016-02-10 12:42:25 +0800

    added textInput to notescreen.js

--- App/Components/NoteScreen.js
+++ App/Components/NoteScreen.js
@@ -2,15 +2,28 @@
 
 import React, {
   StyleSheet,
-  Text,
+  TextInput,
   View
 } from 'react-native';
 
 export default class NoteScreen extends React.Component {
   render() {
     return (
+      //somewhat erroneous here at onEndEditing
       <View style={styles.container}>
-        <Text>Create Note Screen!</Text>
+        <TextInput
+          ref="title"
+          autoFocus={true}
+          placeholder="untitled"
+          onEndEditing={()=>{this.refs.body.focus()}}
+          style={styles.title}
+        />
+        <TextInput
+          ref="body"
+          multiline={true}
+          placeholder="start typing..."
+          style={styles.body}
+        />
       </View>
     );
   }
@@ -21,5 +34,12 @@ var styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    marginTop: 64,
+  },
+  title: {
+    height: 40,
+  },
+  body: {
+    flex: 1,
   }
 })
